92ee47b0f33e91cdd08c2396a980437212e4bb12
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-23 20:42:37 -0300

    Exercicio Básico de orientação Objeto, criando um exemplo de conta de banco

---
+++ src/exercicio01/Conta.java
@@ -0,0 +1,20 @@
+package exercicio01;
+
+public class Conta {
+
+    String nome;
+    int numero;
+    double  saldo;
+
+    //Método para depositar a conta
+
+    public void Depositar(double dinheiro) {
+       saldo+= dinheiro;
+    }
+    //Método para Sacar a conta
+    public void Sacar(double dinheiro) {
+        saldo-= dinheiro;
+    }
+
+}
+
